1c8731361d90af10d94873ca41dc3c25d0f30722
hahalalazy <1936707072@qq.com>
2020-05-21 20:52:15 +0800

    03—_小程序初体验

--- Pages/home/home.js
+++ Pages/home/home.js
@@ -1,66 +1,28 @@
-// Pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'巴啦啦能量',
+    age:18,
+    students:[
+      {id:110,name:'张三',age:23},
+      {id:111,name:'李四',age:24},
+      {id:112,name:'王五',age:25},
+      {id:113,name:'马六',age:26},
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    // 1.错误做法，界面不会刷新
+    // this.data.counter+=1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter:this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter:this.data.counter-1
+    })
   }
 })
